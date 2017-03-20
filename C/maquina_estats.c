daa8eb97cf3b3d2d7b2a5501946ea75b066d8e04
boadajor <boadajor@gmail.com>
2017-03-20 18:08:37 +0100

    LED amb veu

--- C/maquina_estats.c
+++ C/maquina_estats.c
@@ -12,8 +12,18 @@ T_m=(OCR0A+1)/2 us
 #include <stdio.h>
 
 static const uint8_t ocr0a=249; //per fer F_m=8kHz, T_m=125us
-static const int llindar_silenci = 40; //valor maxim de senyal d'entrada considerat com a silenci
+static const int llindar_silenci = 128; //valor maxim de senyal d'entrada considerat com a silenci
 uint8_t input=0;
+
+static int uart_putchar(char c, FILE *stream);
+static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL,_FDEV_SETUP_WRITE);
+static int uart_putchar(char c, FILE *stream){
+  if (c == '\n')
+    uart_putchar('\r', stream);
+  loop_until_bit_is_set(UCSR0A, UDRE0);
+  UDR0 = c;
+  return 0;
+}
 
 static void app_init(){ //inicialitzacio de variables
   setup_ADC(0,5,16);//(adc_input,v_ref,adc_pre)
@@ -23,10 +33,11 @@ static void app_init(){ //inicialitzacio de variables
   setup_tmr0(ocr0a,8);
   serial_init();
   sei();
-  DDRB |= _BV(DDB5);
+  DDRB |= _BV(DDB5);//pin 13 com a sortida per debuggar amb LED
 }
 
 int main(void){
+  stdout = &mystdout;
   app_init();
   while(true){
     printf("%d\n", input);
