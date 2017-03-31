f43df908ebc9ed0a54d6c1dd3d62724e2844200c
boadajor <boadajor@gmail.com>
2017-03-31 15:39:54 +0200

    Pre-potencia

--- C/maquina_estats.c
+++ C/maquina_estats.c
@@ -12,12 +12,23 @@ T_m=(OCR0A+1)/2 us
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum { //Estat de la maquina
+  Data,
+  Silence,
+  Isitdata,
+  Isitsilence,
+} machine_state_t;
+
 static const uint8_t ocr0a=249; //per fer F_m=8kHz, T_m=125us
 static const int llindar_silenci = 28000; //valor maxim de senyal d'entrada considerat com a silenci
 uint8_t input=0; //valor llegit despres de l'ADC
-int segment[50]; //ultimes 50 mostres ->6.25ms
+int segment[50]; // mida de la finestra. Ultimes 50 mostres ->6.25ms
 uint32_t power=0; //potencia
-uint16_t sum=0; //sumatori de les ultimes 50 mostres
+uint8_t sum=0; //compta quants cops entrem a l'estat DATA
+uint8_t counter=0;
+uint8_t index=0;//index de la cua circular
+
+machine_state_t estat=Silence;
 
 static int uart_putchar(char c, FILE *stream);
 static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL,_FDEV_SETUP_WRITE);
@@ -40,37 +51,85 @@ static void app_init(){ //inicialitzacio de variables
   DDRB |= _BV(DDB5);//pin 13 com a sortida per debuggar amb LED
 }
 
+static int calculs(){
+  uint32_t result=0;
+  int i=0;
+  while (i<51){
+    result+=((segment[i]*segment[i])/2);
+    i+=1;
+  }
+  return result; 
+}
+
+static void canvi_estat(){
+  power=calculs();
+  printf("el power val: %ld\n", power);
+  index=0;
+  switch(estat){
+  case Silence:
+    counter=0;
+    PORTB &= ~_BV(PORTB5);
+    if (power>llindar_silenci){
+      counter+=1;
+      estat=Isitdata;
+    }
+    break;
+  case Isitdata:
+    if (power>llindar_silenci){
+      if (counter<5)
+	counter+=1;
+      else
+	estat=Data;
+    }
+    else{
+      counter=1;
+      estat=Isitsilence;
+    }
+    break;
+  case Data:
+    sum+=1;
+    counter=0;
+    PORTB |= _BV(PORTB5);
+    if (power<llindar_silenci){
+      counter+=1;
+      estat=Isitsilence;
+    }
+    else{
+      counter+=1;
+      printf("el counter val: %d\n", counter);
+    }
+    break;
+  case Isitsilence:
+    if (power<llindar_silenci){
+      if (counter<5)
+	counter+=1;
+      else
+	estat=Silence;
+    }
+    else{
+      counter=1;
+      estat=Isitdata;
+    }
+    break;
+  }
+}
+  
+
 int main(void){
   stdout = &mystdout;
   app_init();
-  while(true){
-    power=(sum*sum)/2; //calcul de la potencia
-    printf("%ld\n", power);
-    if (power>llindar_silenci) {
-    //if ((input>llindar_silenci) || (input<(-llindar_silenci))){
-      // counter();
-      /* set pin 5 high to turn led on */
-      PORTB |= _BV(PORTB5);
-    }
-    else{
-      /* set pin 5 low to turn led off */
-      PORTB &= ~_BV(PORTB5);
-    }
+  while(true){      
   }
   return 0;
 }
-
+  
 ISR(TIMER0_COMPA_vect){
-  int i=49;
-  sum=0;
+  input=read8_ADC();  
   start_ADC();
-  input=read8_ADC();
-  while(i>0){
-    segment[i+1]=segment[i]; //desplaçament de mostres
-    sum+=abs(segment[i]); //sumatori de mostres
-    i-=1;
+  segment[index]=input;
+  index+=1;
+  if (index=50){
+    canvi_estat();
+    index=0;
   }
-  segment[1]=segment[0];
-  segment[0]=input;
-  sum+=abs(input);
 }
