1a25a7a07f82a20ada99324ada486ef8e5ef15a7
boadajor <boadajor@gmail.com>
2017-05-03 20:50:20 +0200

    Maquina 2.1 acabada, maquina 3 test

--- C/maquina_estats.c
+++ C/maquina_estats.c
@@ -1,7 +1,7 @@
 /*
-Maquina d'estats per a la deteccio de veu.
-Es configura T mostreig (T_m) amb TMR0.
-T_m=(OCR0A+1)/2 us
+  Maquina d'estats per a la deteccio de veu.
+  Es configura T mostreig (T_m) amb TMR0.
+  T_m=(OCR0A+1)/2 us
 */
 
 #include <avr/interrupt.h>
@@ -13,10 +13,11 @@ T_m=(OCR0A+1)/2 us
 #include <stdlib.h>
 #define MIDA 50 
 #define CONTINUA 128
-#define LLINDAR 28000
-#define VALIDBLOCK 80 //80 mostres de sorol = 500ms de soroll
-#define SHORTPAUSE 80
-#define LONGPAUSE 160
+#define LLINDAR 28000//28000
+#define TS1 80 //80 mostres de silenci = 500ms de silenci
+#define TS2 160
+#define TD1 80
+#define TD2 160
 
 typedef enum { //Estat de la maquina detectora de senyal
   Data,
@@ -32,20 +33,24 @@ typedef enum { //Estat de la maquina detectora de senyal
   Segonsilenci
 } state_t;
 
+typedef enum { //Estat de la maquina detectora de senyal
+  ON,
+  OFF
+} state3_t;
+
 static const uint8_t ocr0a=249; //per fer F_m=8kHz, T_m=125us
 uint8_t input=0; //valor llegit despres de l'ADC
 uint8_t segment[MIDA]; // mida de la finestra. Ultimes 50 mostres ->6.25ms
 volatile int32_t power=0; //potencia
-volatile int32_t power_copy=0; //potencia
-uint8_t ncounter=0; //controla la transicio d'estats de la maquina 1
-uint8_t scounter=0; //controla la transicio d'estats de la maquina 1
+volatile int32_t power2=0; //copia de la potencia
 uint8_t index=0;//index de la cua circular
-bool flag_int=false;
+volatile bool flag_int=false;
 bool flag_silenci=false;
 bool flag_data=false;
 
 machine_state_t estat=Silence;
-state_t state=Inicial;
+state_t estat2=Inicial;
+state3_t estat3=OFF;
 
 static int uart_putchar(char c, FILE *stream);
 static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL,_FDEV_SETUP_WRITE);
@@ -70,9 +75,6 @@ static void app_init(){ //inicialitzacio de variables
   DDRD |=(1<<DDD5);
   DDRD |=(1<<DDD6);
   DDRD |=(1<<DDD7);
-}
-
-static void canvi_estat(){
 
 }
   
@@ -80,66 +82,150 @@ static void canvi_estat(){
 int main(void){
   stdout = &mystdout;
   app_init();
-  int aux_counter=0;
-  if (flag_int){
-    switch(estat){
-    case Silence:
-      serial_put('0');
-      PORTB &= ~_BV(PORTB5);
-      scounter+=1;
-      if (power2>LLINDAR){
-	aux_counter=1;
-	estat=Isitdata;
-      }
-      break;
-    case Isitdata:
-      serial_put('+');
-      scounter+=1;
-      if (power2>LLINDAR){
-	if (aux_counter<5)
-	  aux_counter+=1;
-	else{
-	  flag_silenci=false;
-	  flag_data=true;
-	  estat=Data;
-	  aux_counter=0;
-	  ncounter=0;
+  uint8_t aux_counter=0;
+  uint8_t ts=0; //controla temps a la maquina2
+  uint8_t td=0; //controla temps a la maquina2
+  bool flag_pols=false;
+  while(true){
+    /*
+      /MAQUINA 1:
+    */
+    if (flag_int){
+      switch(estat){
+      case Silence:
+	//serial_put('0');
+	//PORTB &= ~_BV(PORTB5);
+	flag_silenci=false;
+	if (power2>LLINDAR){
+	  aux_counter=1;
+	  estat=Isitdata;
 	}
-      }
-      else{
-	estat=Silence;
-	aux_counter=0;
-      }
-      break;
-    case Data:
-      serial_put('1');
-      ncounter+=1;
-      PORTB |= _BV(PORTB5);
-      if (power2<LLINDAR){
-	aux_counter=1;
-	estat=Isitsilence;
-      }
-      break;
-    case Isitsilence:
-      serial_put('-');
-      ncounter+=1;
-      if (power2<LLINDAR){
-	if (aux_counter<5)
-	  aux_counter+=1;
+	break;
+      case Isitdata:
+	//serial_put('+');
+	if (power2>LLINDAR){
+	  if (aux_counter<5)
+	    aux_counter+=1;
+	  else{
+	    //serial_put('1');
+	    flag_data=true;
+	    estat=Data;
+	    aux_counter=0;
+	  }
+	}
 	else{
-	  flag_silenci=true;
-	  flag_data=false;
 	  estat=Silence;
-	  scounter=0;
 	  aux_counter=0;
 	}
+	break;
+      case Data:
+	//serial_put('1');
+	//PORTB |= _BV(PORTB5);
+	flag_data=false;
+	if (power2<LLINDAR){
+	  aux_counter=1;
+	  estat=Isitsilence;
+	}
+	break;
+      case Isitsilence:
+	//serial_put('-');
+	if (power2<LLINDAR){
+	  if (aux_counter<5)
+	    aux_counter+=1;
+	  else{
+	    //serial_put('0');
+	    flag_silenci=true;
+	    estat=Silence;
+	    aux_counter=0;
+	  }
+	}
+	else{
+	  estat=Data;
+	}
+	break;
       }
-      else{
-	estat=Data;
+      /*
+	/MAQUINA 2:
+      */
+      switch(estat2){
+      case Inicial:
+	if(flag_silenci){
+	  estat2=Primersilenci;
+	  serial_put('a');
+	  ts=0;
+	}
+	break;
+      case Primersilenci:
+	flag_pols=false;
+	ts+=1;
+	if(flag_data){
+	  if(ts>TS1){
+	    estat2=Block;
+	    serial_put('B');
+	    td=0;
+	  } 
+	  else{
+	    estat2=Inicial;
+	    serial_put('I');
+	  }
+	}
+	break;
+      case Block:
+	td+=1;
+	if(td>TD2){
+	  estat2=Inicial;
+	  serial_put('I');
+	}
+	else{
+	  if(flag_silenci){
+	    ts=0;
+	    if(td>TD1){
+	      estat2=Segonsilenci;
+	      serial_put('2');
+	    }
+	    else{
+	      estat2=Primersilenci;
+	      serial_put('b');
+	    }
+	  }
+	}
+	break;
+      case Segonsilenci:
+	ts+=1;
+	if(ts>TS2){
+	  estat2=Primersilenci;
+	  serial_put('c');
+	  ts=0;
+	  //ACCIO!!
+	  flag_pols=true;
+	}
+	else{
+	  if(flag_data){
+	    estat2=Inicial;
+	    serial_put('I');
+	  }
+	}
+	break;
       }
-      break;
+      /*
+	/MAQUINA 3:
+      */
+      switch(estat3){
+      case ON:
+	if(flag_pols){
+	  estat3=OFF;
+	  PORTB |= _BV(PORTB5);
+	}
+	break;
+	case OFF:
+	if(flag_pols){
+	  estat3=ON;
+	  PORTB &= ~_BV(PORTB5);
+	}
+	break;
+      }
+      flag_int=false;
     }
-    flag_int=false;
   }
   return 0;
 }
